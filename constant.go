4bab367e29a0dddd09e9d9115d588d6ce2d02802
Will Faught <will.faught@gmail.com>
2016-04-04 23:39:28 -0700

    Add doc to Constant and funcs

--- constant.go
+++ constant.go
@@ -5,32 +5,41 @@ import (
 	"fmt"
 )
 
+// Constant is a scalar literal.
 type Constant string
 
+// ConstantBoolean returns a Constant for b.
 func ConstantBoolean(b bool) Constant {
 	return Constant(fmt.Sprint(b))
 }
 
+// ConstantInteger returns a Constant for i.
 func ConstantInteger(i int64) Constant {
 	return Constant(fmt.Sprint(i))
 }
 
+// ConstantFloat returns a Constant for f.
 func ConstantFloat(f float64) Constant {
 	return Constant(fmt.Sprint(f))
 }
 
+// ConstantHex returns a Constant for b prefixed by 0x. Two characters represent
+// each byte.
 func ConstantHex(b []byte) Constant {
 	return Constant(fmt.Sprintf("0x%v", hex.EncodeToString(b)))
 }
 
+// ConstantString returns a Constant for s.
 func ConstantString(s string) Constant {
 	return Constant(fmt.Sprintf("'%v'", s))
 }
 
+// ConstantStringEscaped returns a Constant for s.
 func ConstantStringEscaped(s string) Constant {
 	return Constant(fmt.Sprintf("$$%v$$", s))
 }
 
+// ConstantUUID returns a Constant for uuid.
 func ConstantUUID(s string) Constant {
 	return Constant(fmt.Sprint(s))
 }
