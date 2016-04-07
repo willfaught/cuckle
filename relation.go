3f61a29dc0321ddaf3b0f0992acb84a5cd44df86
Will Faught <will.faught@gmail.com>
2016-04-06 18:26:46 -0700

    Add Order and func doc

--- relation.go
+++ relation.go
@@ -2,8 +2,10 @@ package cuckle
 
 import "fmt"
 
+// Relation is an infix operation with an operator and two operands.
 type Relation string
 
+// NewRelation returns a new Relation for left, o, and right.
 func NewRelation(left Term, o Operator, right Term) Relation {
 	return Relation(fmt.Sprintf("%v %v %v", left, o, right))
 }
