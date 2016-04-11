2f87cb090e52c289782dcc54a9472ba0e91ecf10
Will Faught <will.faught@gmail.com>
2016-04-10 20:59:42 -0700

    Add Variable type, const, func doc

--- variable.go
+++ variable.go
@@ -2,10 +2,13 @@ package cuckle
 
 import "fmt"
 
+// Variable is a CQL variable.
 type Variable string
 
+// VariableAnonymous is anonymous.
 const VariableAnonymous Variable = "?"
 
+// NewVariable returns a Variable for a name.
 func NewVariable(name string) Variable {
 	return Variable(fmt.Sprintf(":%v", name))
 }
