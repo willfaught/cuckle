47247492743295aa065ede8328633466edf4a8c9
Will Faught <will.faught@gmail.com>
2016-04-10 20:56:43 -0700

    Add Type, consts, and funcs doc

--- type.go
+++ type.go
@@ -5,8 +5,10 @@ import (
 	"strings"
 )
 
+// Type is a type.
 type Type string
 
+// Built-in types.
 const (
 	TypeAscii     Type = "ascii"
 	TypeBigint    Type = "bigint"
@@ -30,18 +32,22 @@ const (
 	TypeVarint    Type = "varint"
 )
 
+// TypeList returns a Type for a list of element.
 func TypeList(element Type) Type {
 	return Type(fmt.Sprintf("list<%v>", element))
 }
 
+// TypeMap returns a Type for a map from key to value.
 func TypeMap(key, value Type) Type {
 	return Type(fmt.Sprintf("map<%v, %v>", key, value))
 }
 
+// TypeSet returns a Type for a set of element.
 func TypeSet(element Type) Type {
 	return Type(fmt.Sprintf("set<%v>", element))
 }
 
+// TypleTuple returns a Type for a tuple of elements.
 func TypeTuple(elements ...Type) Type {
 	var ss []string
 
